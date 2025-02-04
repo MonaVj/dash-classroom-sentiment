9e5d3ac2790e38727cb26daeb133fe18cbc7a761
MonaVj <92733303+MonaVj@users.noreply.github.com>
2025-02-04 18:02:55 -0500

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,12 @@ st.markdown(
 )
 
 # File Upload
-uploaded_file = st.file_uploader("Upload your dataset (CSV format)", type=["csv"])
+if "uploaded_file" not in st.session_state:
+    uploaded_file = st.file_uploader("Upload your dataset (CSV format)", type=["csv"])
+    if uploaded_file:
+        st.session_state["uploaded_file"] = uploaded_file
+else:
+    uploaded_file = st.session_state["uploaded_file"]
 
 if uploaded_file:
     try:
@@ -54,10 +59,6 @@ if uploaded_file:
                 Longitude=("Longitude", "mean"),
                 Count=("Count", "sum"),
             ).reset_index()
-
-            # Preview Data
-            st.write("### Data Preview")
-            st.dataframe(df)
 
             # Section: Overall Sentiment Analysis Map
             st.markdown(
